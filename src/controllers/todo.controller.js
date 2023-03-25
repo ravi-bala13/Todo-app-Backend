dd9113a63123003498e5e8af63f9882d9ea238df
BalaMurugan <bals161616@gmail.com>
2023-03-25 20:20:20 +0530

    cors added

--- src/controllers/todo.controller.js
+++ src/controllers/todo.controller.js
@@ -13,13 +13,14 @@ router.get("/", async(req, res) => {
     }
 })
 
-// router.post("/", async(req, res) => {
-//     try{
-//         const todos = await Todos.find().lean().exec()
-//         return res.status(200).send(todos)
-//     }catch(e){
-//         return res.status(500).json({message: e.message, status: "Failed"})
-//     }
-// })
+router.post("/", async(req, res) => {
+    try{
+        console.log(req.body)
+        const todo = await Todos.create(req.body)
+        return res.status(200).send({message: "success fully updated todo"})
+    }catch(e){
+        return res.status(500).json({message: e.message, status: "Failed"})
+    }
+})
 
 module.exports = router
